4d722a9431c951a5267bc9fc37d403c56de7877c
chaeeon <chaeeon37@naver.com>
2022-03-19 22:32:04 +0900

    first

---
+++ 투두리스트.js
@@ -0,0 +1,42 @@
+// const todoForm = document.querySelector("#todo_form");
+// const todoInput = document.querySelector("#todo_form input");
+// const todoBtn = document.querySelector("todo_button");
+// const todoBoard = document.querySelector("#todo-board")
+
+
+// todoInput.style.backgroundColor = "red";
+// todoBtn.addEventListener("click",todoBt);
+
+// function todoBt() {
+//     const li = document.createElement("li");
+//    const checkBox = document.createElement("input");
+//    checkBox.type = "checkbox";
+//    checkBox.addEventListener("click".todoCheck);
+
+//    const text  =document.createTextNode(todoInput.value);
+
+
+//    const button = document.createElement("button");
+//    button.textContent = "X";
+//    button.addEventListener("click",todoDelete);
+
+//    li.appendChild(checkBox);
+//    li.appendChild(text);
+//    li.appendChild(button);
+
+//    todoBoard.appendChild(li);
+//    todoInput.value="";
+// }
+
+// function todoCheck(e) {
+//     const checkBox = e.target;
+//     if(checkBox.checked) {
+//         checkBox.parentNode.sytle.color ="gray";
+//     } else{
+//         checkBox.parentNode.sytle.color ="black";
+//     }
+// }
+
+// function todoDelete(e) {
+//     e.target.parentNode.remove();
+// }
